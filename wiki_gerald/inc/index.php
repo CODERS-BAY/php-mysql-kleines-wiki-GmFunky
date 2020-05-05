b664cb5657e9e9834e0d321cd919efa434a9c6be
Gerald <gerald.mares@gmail.com>
2020-05-05 09:37:53 +0200

    update Auto Wiki

---
+++ wiki_gerald/inc/index.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html>
+
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Wiki Gerald</title>
+      <link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&family=Shrikhand&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" href="../css/wiki.css">
+</head>
+
+<body class="back">
+      <nav class="navbar navbar-expand-lg  navbar-dark bg-dark  fixed-top">
+            <a class="navbar-brand " id="projekte" href="Gery.html">Dreamcars</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto text-center">
+                        <li class="nav-item">
+                              <a class="nav-link px-4" href="">
+                                    content1</a>
+                        </li>
+                        <li class="nav-item">
+                              <a class="nav-link px-4" href="">
+                                    content2</a>
+                        </li>
+                        <li class="nav-item">
+                              <a class="nav-link px-4" href="">
+                                    content3</a>
+                        </li>
+                  </ul>
+            </div>
+      </nav>
+
+      <section class="sliderSectionStyle">
+            <div id="carouselExampleCaptions" class="carousel slide  " data-ride="carousel">
+                  <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                  </ol>
+                  <div class="carousel-inner">
+                        <div class="carousel-item active">
+                              <img src="../img/slider1.jpg" class="d-block w-100 " alt="bild">
+                              <div class="carousel-caption d-none d-md-block ">
+                                    <h5 class="sliderText1">Dream Cars Wiki</h5>
+                                    <p class="sliderText2">Die schönsten Autos der Welt </p>
+                              </div>
+                        </div>
+                        <div class="carousel-item">
+                              <img src="../img/slider2.jpg" class="d-block w-100" alt="bild">
+                              <div class="carousel-caption d-none d-md-block">
+                                    <h5 class="sliderText1">Dream Cars Wiki</h5>
+                                    <p class="sliderText2">.</p>
+                              </div>
+                        </div>
+                        <div class="carousel-item">
+                              <img src="../img/slider3.jpg" class="d-block w-100" alt="bild">
+                              <div class="carousel-caption d-none d-md-block">
+                                    <h5 class="sliderText1">Dream Cars Wiki</h5>
+                                    <p class="sliderText2"></p>
+                              </div>
+                        </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                  </a>
+            </div>
+      </section>
+
+      <section class="cardSectionStyle">
+            <h1 class="h1StyleAutoLekikon">Das Große Autolexikon</h1>
+            <div class="container">
+                  <div class="card-columns">
+                        <div class="card">
+                              <img src="../img/card1.jpg" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                    <h5 class="card-title">Porsche 911</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural
+                                          lead-in to additional </p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+                        </div>
+                        <div class="card">
+                              <img src="../img/card3.jpg" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                    <h5 class="card-title">Mini Cooper</h5>
+                                    <p class="card-text">This card has supporting text below as a natural lead-in to
+                                          additional content.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+
+                        </div>
+                        <div class="card">
+                              <img src="../img/card2.jpg" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                    <h5 class="card-title">Chevy</h5>
+                                    <p class="card-text">This card has supporting text below as a natural lead-in to
+                                          additional content.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+                        </div>
+                        <div class="card">
+                              <img src="../img/card1.jpg" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                    <h5 class="card-title">Porsche 911</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural
+                                          lead-in to additional </p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+                        </div>
+                        <div class="card">
+                              <img src="../img/card3.jpg" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                    <h5 class="card-title">Mini Cooper</h5>
+                                    <p class="card-text">This card has supporting text below as a natural lead-in to
+                                          additional content.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+
+                        </div>
+                        <div class="card">
+                              <img src="../img/card2.jpg" class="card-img-top" alt="...">
+                              <div class="card-body">
+                                    <h5 class="card-title">Chevy</h5>
+                                    <p class="card-text">This card has supporting text below as a natural lead-in to
+                                          additional content.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      </section>
+      
+      <!--ModaL
+      <div class="modal fade" id="showModal" tabindex="-1" role="dialog" aria-labelledby="lexikon-entry"
+            aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                  <div class="modal-content">
+
+                        <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                  </div>
+            </div>
+      </div>
+      Info Ende-->
+
+     
+      <?php
+
+      /*
+      include('inc/login.inc.php');
+      $result = $con->query("SELECT id, title, teaser, imgpath from content");
+
+      while($entry = $result->fetch_assoc())
+           {    
+      echo
+        
+        $dsatz[""] . ", "
+      . $dsatz[""] . ", "
+      . $dsatz[""] . " ";
+      
+      }*/
+      ?>
+
+      <?php
+      /*
+      if($entry['imgpath']) ?>
+      src="lexikon-img/<?php echo $entry['imgpath'] ?>"
+      <button type="button" class="card-title btn ajaxModal" data-toggle="modal" data-id="<?php echo $entry['id'] ?>">
+            <?php echo $entry['teaser']; ?>
+            <?php $con->close();*/ ?>
+
+
+
+            <script src="https://code.jquery.com/jquery-3.5.0.min.js"
+                  integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+                  integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+                  crossorigin="anonymous">
+            </script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+                  integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+                  crossorigin="anonymous">
+            </script>
+
+</body>
+
+</html>
